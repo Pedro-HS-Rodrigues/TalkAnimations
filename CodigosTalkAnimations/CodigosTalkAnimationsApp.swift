3a583c465e16de9f87279baa2cdf48cbddffa95d
PedroHenriqueSantosRodrigues <pedrohenrique10001@gmail.com>
2023-07-31 19:30:27 -0300

    Initial Commit

---
+++ CodigosTalkAnimations/CodigosTalkAnimationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  CodigosTalkAnimationsApp.swift
+//  CodigosTalkAnimations
+//
+//  Created by Pedro Henrique Santos Rodrigues on 31/07/23.
+//
+
+import SwiftUI
+
+@main
+struct CodigosTalkAnimationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
